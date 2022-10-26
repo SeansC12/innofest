220e0c47eb9eab14af580dfc051b44277e4f085b
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2022-10-26 10:21:03 +0800

    elevator layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Artyom from "artyom.js";
+import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+
+const Friday = new Artyom();
 
 function App() {
+  const [floor, setFloor] = useState(0);
+  const textRef = useRef();
+
+  useEffect(() => {
+    console.log(floor);
+  }, [floor]);
+
+  Friday.addCommands([
+    {
+      indexes: ["Go to floor *"],
+      smart: true,
+      action: (i, wildcard) => {
+        console.log(wildcard);
+        setFloor(wildcard);
+        Friday.say("Going to floor" + wildcard);
+      },
+    },
+    {
+      indexes: ["Hello"],
+      action: (i) => {
+        Friday.say("Hi, how is it going");
+      },
+    },
+  ]);
+
+  function initialiseFriday() {
+    Friday.initialize({
+      lang: "en-GB",
+      continuous: true,
+      debug: true,
+      listen: true,
+      soundex: true,
+      // name: "Friday",
+    });
+  }
+
+  Friday.redirectRecognizedTextOutput((text, isFinal) => {
+    if (isFinal) {
+      console.log(text);
+    }
+  });
+
+  function killFriday() {
+    Friday.fatality();
+  }
+
+  const elevatorAnimation = {
+    y: -20,
+    transition: {
+      type: "tween",
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="buttons">
+        <div>
+          <button onClick={initialiseFriday}>Start listening</button>
+          <button onClick={killFriday}>Kill</button>
+        </div>
+        <div ref={textRef}>Go to {floor}</div>
+      </div>
+      <div className="elevatorWrapper">
+        <div className="elevatorTracks">
+          <motion.div
+            // initial={{ x: 0, y: 0 }}
+            animate={elevatorAnimation}
+            className="elevator"
+          ></motion.div>
+        </div>
+      </div>
     </div>
   );
 }
